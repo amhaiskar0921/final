7a1308a7412828941538a69ac6a25ecc584e3a72
Arya Mhaiskar <mhaiskaa@uci.edu>
2024-04-26 23:08:41 -0700

    Added filterBy

--- src/components/display.jsx
+++ src/components/display.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
 import { supabase } from '../client';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, Form, FormControl, Card } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Card, Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import './display.css';
 
 function Display() {
   const [posts, setPosts] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [sortType, setSortType] = useState(null);
 
   const handleSearch = (event) => {
     setSearchQuery(event.target.value);
@@ -15,20 +15,26 @@ function Display() {
   };
 
   useEffect(() => {
+    const fetchPosts = async () => {
+      let query = supabase.from('posts').select('*');
+
+      if (sortType === 'date') {
+        query = query.order('created_at', { ascending: false });
+      } else if (sortType === 'upvotes') {
+        query = query.order('num_upvotes', { ascending: false });
+      }
+
+      const { data, error } = await query;
+
+      if (error) {
+        console.error('Error fetching posts:', error);
+      } else {
+        setPosts(data);
+      }
+    };
+
     fetchPosts();
-  }, []);
-
-  async function fetchPosts() {
-    const { data, error } = await supabase
-      .from('posts')
-      .select('*');
-
-    if (error) {
-      console.error('Error: ', error);
-    } else {
-      setPosts(data);
-    }
-  }
+  }, [sortType]);
 
   return (
     <div className='post-gallery'>
@@ -37,10 +43,15 @@ function Display() {
         <Nav className="mr-auto">
           <Nav.Link href="/create">Add HotTake</Nav.Link>
         </Nav>
+
         <Form inline="true">
           <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={handleSearch} />
         </Form>
       </Navbar>
+
+      <Button variant="outline-primary" onClick={() => setSortType('date')}>Sort by Date</Button>
+      <Button variant="outline-primary" onClick={() => setSortType('upvotes')} className="ml-2">Sort by Upvotes</Button>
+
       {posts.filter(post => post.title.includes(searchQuery)).map((post) => (
         <LinkContainer key={post.id} to={`/posts/${post.id}`}>
           <Card className='post-card'>
